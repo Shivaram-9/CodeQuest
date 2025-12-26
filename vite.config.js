8797a8b89fc3ba96e6c8efcd0b5abdb48355eede
Shivaram9 <ymvshiva1784@gmail.com>
2025-12-26 10:09:06 +0530

    Update base path comment in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CodeQuest/',   // 🔴 MUST MATCH REPO NAME
+  base: '/CodeQuest/',   // repo name EXACT
   plugins: [react()],
 })
